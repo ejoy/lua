595ada217624268e92a56b4676e2a47223b563ae
Cloud Wu <cloudwu@gmail.com>
2019-06-06 14:28:41 +0800

    update

--- src/lstring.c
+++ src/lstring.c
@@ -230,9 +230,10 @@ struct shrmap {
 
 static struct shrmap SSM;
 
-#define ADD_SREF(ts) ATOM_INC(&((ts)->u.ref))
-#define DEC_SREF(ts) ATOM_DEC(&((ts)->u.ref))
+#define ADD_SREF(ts) do {if(ATOM_INC(&((ts)->u.ref))==1) ATOM_DEC(&SSM.garbage);}while(0)
+#define DEC_SREF(ts) do {if(ATOM_DEC(&((ts)->u.ref))==0) ATOM_INC(&SSM.garbage);}while(0)
 #define ZERO_SREF(ts) ((ts)->u.ref == 0)
+#define FREE_SREF(ts) do {free(ts);ATOM_DEC(&SSM.total);ATOM_DEC(&SSM.garbage);}while(0)
 
 static struct ssm_ref *
 newref(int size) {
@@ -335,8 +336,11 @@ markref(struct ssm_ref *r, TString *s, int changeref) {
 	unsigned int h = s->hash;
 	int slot = lmod(h, r->hsize);
 	TString * hs = r->hash[slot];
-	if (hs == s)
+	if (hs == s){
+		if (changeref)
+			DEC_SREF(s);
 		return;
+	}
 	++r->nuse;
 	if (r->nuse >= r->hsize && r->hsize <= MAX_INT/2) {
 		expand_ref(r, changeref);
@@ -346,6 +350,8 @@ markref(struct ssm_ref *r, TString *s, int changeref) {
 	if (hs != NULL) {
 		if (hs == s) {
 			--r->nuse;
+			if (changeref)
+				DEC_SREF(s);
 			return;
 		}
 		insert_ref(r, hs);
@@ -425,12 +431,12 @@ mergeset(struct ssm_ref *set, struct ssm_ref * rset, int changeref) {
 	for (i=0;i<rset->hsize;i++) {
 		TString * s = rset->hash[i];
 		if (s) {
-			insert_ref(set, s);
+			markref(set, s, changeref);
 		}
 	}
 	for (i=0;i<rset->asize;i++) {
 		TString * s = rset->array[i];
-		insert_ref(set, s);
+		markref(set, s, changeref);
 	}
 	delete_ref(rset);
 	remove_duplicate(set, changeref);
@@ -518,14 +524,6 @@ exist(struct ssm_ref *r, TString *s) {
 	return 0;
 }
 
-static void
-release_tstring(TString *s) {
-	DEC_SREF(s);
-	if (ZERO_SREF(s)) {
-		ATOM_INC(&SSM.garbage);
-	}
-}
-
 static int
 collectref(struct collect_queue * c) {
 	int i;
@@ -544,7 +542,8 @@ collectref(struct collect_queue * c) {
 			if (s) {
 				if (!exist(mark, s) && !exist(fix, s)) {
 					save->hash[i] = NULL;
-					release_tstring(s);
+					--save->nuse;
+					DEC_SREF(s);
 					++total;
 				}
 			}
@@ -554,8 +553,9 @@ collectref(struct collect_queue * c) {
 			TString * s = save->array[i];
 			if (!exist(mark, s) && !exist(fix, s)) {
 				--save->asize;
+				--save->nuse;
 				save->array[i] = save->array[save->asize];
-				release_tstring(s);
+				DEC_SREF(s);
 				++total;
 			} else {
 				++i;
@@ -566,13 +566,13 @@ collectref(struct collect_queue * c) {
 		for (i=0;i<save->hsize;i++) {
 			TString * s = save->hash[i];
 			if (s) {
-				release_tstring(s);
+				DEC_SREF(s);
 				++total;
 			}
 		}
 		for (i=0;i<save->asize;i++) {
 			TString * s = save->array[i];
-			release_tstring(s);
+			DEC_SREF(s);
 			++total;
 		}
 		clear_vm(c);
@@ -659,7 +659,7 @@ shrstr_deletepage(struct shrmap_slot *s, int sz) {
 			TString *str = s[i].str;
 			while (str) {
 				TString * next = (TString *)str->next;
-				free(str);
+				FREE_SREF(str);
 				str = next;
 			}
 		}
@@ -689,9 +689,7 @@ shrstr_rehash(struct shrmap *s, int slotid) {
 		while (str) {
 			TString * next = (TString *)str->next;
 			if (ZERO_SREF(str)) {
-				free(str);
-				ATOM_DEC(&SSM.total);
-				ATOM_DEC(&SSM.garbage);
+				FREE_SREF(str);
 			} else {
 				int newslotid = lmod(str->hash, s->rwslots);
 				struct shrmap_slot *newslot = &s->readwrite[newslotid];
@@ -772,10 +770,8 @@ sweep_slot(struct shrmap *s, int i) {
 		while (ts) {
 			if (ZERO_SREF(ts)) {
 				*ref = (TString *)ts->next;
-				free(ts);
+				FREE_SREF(ts);
 				ts = *ref;
-				ATOM_DEC(&SSM.total);
-				ATOM_DEC(&SSM.garbage);
 				++n;
 			} else {
 				ref = (TString **)&(ts->next);
@@ -891,18 +887,13 @@ find_and_collect(struct shrmap_slot * slot, unsigned int h, const char *str, lu_
 		if (ts->hash == h &&
 			ts->shrlen == l &&
 			memcmp(str, ts+1, l) == 0) {
-			if (ZERO_SREF(ts)) {
-				ATOM_INC(&SSM.garbage);
-			}
 			ADD_SREF(ts);
 			break;
 		}
 		if (ZERO_SREF(ts)) {
 			*ref = (TString *)ts->next;
-			free(ts);
+			FREE_SREF(ts);
 			ts = *ref;
-			ATOM_DEC(&SSM.total);
-			ATOM_DEC(&SSM.garbage);
 		} else {
 			ref = (TString **)&(ts->next);
 			ts = *ref;
@@ -945,7 +936,6 @@ new_string(unsigned int h, const char *str, lu_byte l) {
 	memset(ts, 0, sz);
 	setbits(ts->marked, WHITEBITS);
 	gray2black(ts);
-	makeshared(ts);
 	ts->tt = LUA_TSHRSTR;
 	ts->hash = h;
 	ts->shrlen = l;
